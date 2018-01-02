2ae36d77fde59f1460177abab32d2028d7318e83
InnaMel <InnaMel@users.noreply.github.com>
2018-01-02 15:03:42 +0000

    20180102T150316Z

--- mario.c
+++ mario.c
@@ -1,57 +1,50 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (void)
-{
+int main (void){
   int height = 0;
 
   //started cycle "do while"
-  do
-    {
-      printf ("Height: ");
+  do {
+    printf ("Height: ");
 
-      //getting height for pyramid
-      height = get_int ();
-    }
+    //getting height for pyramid
+    height = get_int ();
+  }
   while ((height < 0) || (height > 23));
 
   //switching our actions depending on conditions
-  switch (height)
+  switch (height) {
+
+    // if height is zero - doing nothing
+  case 0:
+    break;
+
+  default:
     {
 
-      // if height is zero - doing nothing
-      case 0:
-      break;
+      //printing pyramid
+      int space = height - 1;
+      for (int i = 0; i < height; i++) {
+	for (int j = 0; j < (height + 1); j++) {
 
-      default:
-        {
+	  //if row less space - print white space
+	  if (j < space) {
+	    printf (" ");
+	  }
 
-          //printing pyramid
-          int space = height - 1;
-          for (int i = 0; i < height; i++)
-          {
-            for (int j = 0; j < (height + 1); j++)
-            {
+	  //if space less row  - print #
+	  else {
+	    printf ("#");
+	  }
+	}
 
-              //if row less space - print white space
-              if (j < space)
-                {
-                  printf (" ");
-                }
+	//decrement space
+	space--;
 
-              //if space less row  - print #
-              else
-                {
-                  printf ("#");
-                }
-            }
-
-            //decrement space
-            space--;
-
-            //line translation
-            printf ("\n");
-          }
-        }
+	//line translation
+	printf ("\n");
+      }
     }
+  }
 }
