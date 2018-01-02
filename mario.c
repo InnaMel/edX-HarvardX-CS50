b06c63c27fb18ace2b106d3804433148bb78b1ff
InnaMel <InnaMel@users.noreply.github.com>
2018-01-02 09:59:12 +0000

    20180102T095903Z

--- mario.c
+++ mario.c
@@ -1,27 +1,44 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (void) {
+int main (void)
+
+{
   int height = 0;
-  do {
+  do
+
+  {
     printf ("Height: ");
     height = get_int ();
   }
   while ((height < 0) || (height > 23));
-  switch (height) {
+  switch (height)
+
+  {
   case 0:
     break;
   default:
     {
       int space = height - 1;
-      for (int i = 0; i < height; i++) {
-        for (int j = 0; j < (height + 1); j++) {
-          if (j < space) {
+      for (int i = 0; i < height; i++)
+
+      {
+        for (int j = 0; j < (height + 1); j++)
+
+        {
+          if (j < space)
+
+          {
             printf (" ");
           }
           else
+
+          {
             printf ("#");
+          }
         }
+
+
         space--;
         printf ("\n");
       }
