d68ccfb331cdcaaee17d0b93b58cde159d1cb16e
InnaMel <InnaMel@users.noreply.github.com>
2018-01-02 10:21:56 +0000

    20180102T102148Z

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
-#include <cs50.h>
+//#include <cs50.h>
 #include <stdio.h>
 
 
 int main (void)
 {
+    //this function printing any data, in this case this is just txt
     printf("hello, world\n");
 }
